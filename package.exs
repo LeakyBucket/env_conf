2a9e42b42b71a7f4c8df013e9332daeac8186377
Glen <DamnBigMan@gmail.com>
2014-02-10 13:30:06 -0700

    Expm

---
+++ package.exs
@@ -0,0 +1,11 @@
+Expm.Package.new(
+  name: "env_conf", 
+  description: "Environment Backed Configuration Service for Elixir",
+  homepage: "https://github.com/LeakyBucket/env_conf",
+  version: "0.1.0", 
+  keywords: ["configuration", "service", "environment"], 
+  licenses: [[name: "MIT"]],
+  maintainers: [[name: "Glen Holcomb", 
+                email: "DamnBigMan@gmail.com"]],
+  repositories: [[github: "LeakyBucket/env_conf"]]
+)
