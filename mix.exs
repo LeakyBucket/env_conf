73a5d04427f6b8b5e67467cb95dac4eba9f86187
Glen Holcomb <leakybucket@Glens-MacBook-Pro.local>
2014-08-03 20:13:34 -0600

    Moving from expm to hex.

--- mix.exs
+++ mix.exs
@@ -2,15 +2,30 @@ defmodule EnvConf.Mixfile do
   use Mix.Project
 
   def project do
-    [ app: :env_conf,
+    [app: :env_conf,
       version: "0.1.0",
       elixir: "~> 0.14.3",
-      deps: deps(Mix.env) ]
+      description: description,
+      package: package,
+      deps: deps(Mix.env)]
   end
 
   # Configuration for the OTP application
   def application do
     [mod: { EnvConf, [] }]
+  end
+
+  defp description do
+    """
+      A simple 12-Factor configuration service for Elixir.
+    """
+  end
+
+  defp package do
+    [ files: ["lib", "docs", "test", "mix.exs", "README.*", "LICENSE"],
+      contributors: ["Glen Holcomb"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/leakybucket/env_conf.git"}]
   end
 
   # Returns the list of dependencies in the format:
